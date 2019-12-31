f9fb44cc33eced2345b44f20dd8abd1c3ab9554e
Sergio Pantoja <2530982+sergiopantoja@users.noreply.github.com>
2019-12-30 19:11:18 -0500

    Update homepage with dope art

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,10 +13,8 @@ require("channels")
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+const images = require.context('../images', true)
+const imagePath = (name) => images(name, true)
 
 import "controllers"
 import "stylesheets/application"
-import "images/logo"
-import "images/cashflow"
